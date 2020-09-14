a3a9a5f2987919be70991be0a2ace14aa62233f6
tracer-pa-public <tracer@njuics.org>
2020-09-14 22:42:07 +0800

    >  compile nemu testcase kernel 191220177 pa191220177 Linux 32e3186b035e 4.15.0-45-generic #48~16.04.1-Ubuntu SMP Tue Jan 29 18:03:48 UTC 2019 x86_64 GNU/Linux  22:42:07 up 7 days, 14:04,  1 user,  load average: 2.14, 2.23, 2.18 3bb4b631a1eaac10c2528c9c234740b52e7841c

--- nemu/src/cpu/alu.c
+++ nemu/src/cpu/alu.c
@@ -295,10 +295,19 @@ uint32_t alu_shr(uint32_t src, uint32_t dest, size_t data_size)
 #ifdef NEMU_REF_ALU
 	return __ref_alu_shr(src, dest, data_size);
 #else
-	printf("\e[0;31mPlease implement me at alu.c\e[0m\n");
+	/*printf("\e[0;31mPlease implement me at alu.c\e[0m\n");
 	fflush(stdout);
 	assert(0);
-	return 0;
+	return 0;*/
+	uint32_t result = dest >> src;
+	result = result & get_mask(data_size);
+	cpu.eflags.CF = 0;
+	if (src <= data_size)
+	    cpu.eflags.CF = dest >> (src - 1) & 1;
+	set_ZF(result,data_size);
+	set_PF(result);
+	set_SF(result,data_size);
+	return result;
 #endif
 }
 
