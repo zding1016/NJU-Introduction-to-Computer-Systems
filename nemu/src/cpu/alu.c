c45377a142d0016cba210cfd1708b2dc35358783
tracer-pa-public <tracer@njuics.org>
2020-09-14 22:36:50 +0800

    >  compile nemu testcase kernel 191220177 pa191220177 Linux 32e3186b035e 4.15.0-45-generic #48~16.04.1-Ubuntu SMP Tue Jan 29 18:03:48 UTC 2019 x86_64 GNU/Linux  22:36:50 up 7 days, 13:59,  1 user,  load average: 2.86, 2.38, 2.19 5dfec9a2a588c4fc3a5f22691c2529c3201e95b

--- nemu/src/cpu/alu.c
+++ nemu/src/cpu/alu.c
@@ -282,10 +282,11 @@ uint32_t alu_shl(uint32_t src, uint32_t dest, size_t data_size)
 #ifdef NEMU_REF_ALU
 	return __ref_alu_shl(src, dest, data_size);
 #else
-	printf("\e[0;31mPlease implement me at alu.c\e[0m\n");
+	/*printf("\e[0;31mPlease implement me at alu.c\e[0m\n");
 	fflush(stdout);
 	assert(0);
-	return 0;
+	return 0;*/
+	return alu_sal(src,dest,data_size);
 #endif
 }
 
@@ -318,9 +319,15 @@ uint32_t alu_sal(uint32_t src, uint32_t dest, size_t data_size)
 #ifdef NEMU_REF_ALU
 	return __ref_alu_sal(src, dest, data_size);
 #else
-	printf("\e[0;31mPlease implement me at alu.c\e[0m\n");
+	/*printf("\e[0;31mPlease implement me at alu.c\e[0m\n");
 	fflush(stdout);
 	assert(0);
-	return 0;
+	return 0;*/
+	uint32_t result = dest << src;
+	cpu.eflags.CF = 0;
+	set_ZF(result,data_size);
+	set_PF(result);
+	set_SF(result,data_size);
+	return result;
 #endif
 }
