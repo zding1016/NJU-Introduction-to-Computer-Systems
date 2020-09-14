49e2f78ce396d003945613a24b31dbc51beddc4a
tracer-pa-public <tracer@njuics.org>
2020-09-14 23:00:18 +0800

    >  compile nemu testcase kernel 191220177 pa191220177 Linux 32e3186b035e 4.15.0-45-generic #48~16.04.1-Ubuntu SMP Tue Jan 29 18:03:48 UTC 2019 x86_64 GNU/Linux  23:00:18 up 7 days, 14:23,  1 user,  load average: 2.06, 2.07, 2.09 9d6955138f15b8b071c63b2ac1ddb97934d0c6c9

--- nemu/src/cpu/alu.c
+++ nemu/src/cpu/alu.c
@@ -321,12 +321,13 @@ uint32_t alu_sar(uint32_t src, uint32_t dest, size_t data_size)
 	fflush(stdout);
 	assert(0);
 	return 0;*/
-	uint32_t high_sign = (dest >> (src - 1)) & 1;
+	uint32_t high_sign = (dest >> (data_size - 1)) & 1;
 	if (!high_sign) return alu_shr(src, dest, data_size);
 	cpu.eflags.CF = 0;
 	if (src <= data_size)
-	    cpu.eflags.CF = high_sign;
+	    cpu.eflags.CF = (dest >> (src - 1)) & 1;
 	uint32_t result = dest;
+	result = result & get_mask(data_size);
 	result = result >> src;
 	uint32_t number_or_for = 0xffffffff << (data_size - src);
 	result = result | number_or_for;
