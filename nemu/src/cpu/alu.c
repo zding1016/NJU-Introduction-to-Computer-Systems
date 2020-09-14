a9ba833cd1b8df8af7fec436c2dd01700ce344eb
tracer-pa-public <tracer@njuics.org>
2020-09-14 22:38:44 +0800

    >  compile nemu testcase kernel 191220177 pa191220177 Linux 32e3186b035e 4.15.0-45-generic #48~16.04.1-Ubuntu SMP Tue Jan 29 18:03:48 UTC 2019 x86_64 GNU/Linux  22:38:44 up 7 days, 14:01,  1 user,  load average: 2.26, 2.36, 2.21 207996374fc1751387a5bf99bf884a703f9c248f

--- nemu/src/cpu/alu.c
+++ nemu/src/cpu/alu.c
@@ -324,7 +324,10 @@ uint32_t alu_sal(uint32_t src, uint32_t dest, size_t data_size)
 	assert(0);
 	return 0;*/
 	uint32_t result = dest << src;
+	result = result & get_mask(data_size); 
 	cpu.eflags.CF = 0;
+	if (src <= data_size) 
+	    cpu.eflags.CF = (dest >> (data_size - src)) & 1;
 	set_ZF(result,data_size);
 	set_PF(result);
 	set_SF(result,data_size);
