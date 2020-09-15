c81e642e50155cf9170a4ab8bb32a7578ac41f90
tracer-pa-public <tracer@njuics.org>
2020-09-15 10:36:56 +0800

    >  compile nemu testcase kernel 191220177 pa191220177 Linux 32e3186b035e 4.15.0-45-generic #48~16.04.1-Ubuntu SMP Tue Jan 29 18:03:48 UTC 2019 x86_64 GNU/Linux  10:36:56 up 8 days,  1:59,  1 user,  load average: 2.32, 2.26, 2.19 eec764bf89bc7d2965ad1ba73db13e20128a1fdf

--- nemu/src/cpu/fpu.c
+++ nemu/src/cpu/fpu.c
@@ -108,7 +108,7 @@ inline uint32_t internal_normalize(uint32_t sign, int32_t exp, uint64_t sig_grs)
 		else if ((last_three_bit == 4) && ((last_three_bit & 0x1) == 0x1)){
 		    sig_grs++;
 		} 
-		if ((sig_grs >> 23 != 1) && exp != 0){
+		if ((sig_grs >> 23 > 1)){
 		    sig_grs = sig_grs >> 1;
 		    exp++;
 		    if (exp >= 0xff){
