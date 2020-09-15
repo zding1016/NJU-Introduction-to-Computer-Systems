00f481521e63c29f30fbef134213f01838869c68
tracer-pa-public <tracer@njuics.org>
2020-09-15 10:51:16 +0800

    >  compile nemu testcase kernel 191220177 pa191220177 Linux 32e3186b035e 4.15.0-45-generic #48~16.04.1-Ubuntu SMP Tue Jan 29 18:03:48 UTC 2019 x86_64 GNU/Linux  10:51:16 up 8 days,  2:14,  1 user,  load average: 2.24, 2.14, 2.10 7b8260fb658f1fa3627c4f3389d8e5b4cb8891e0

--- nemu/src/cpu/fpu.c
+++ nemu/src/cpu/fpu.c
@@ -324,7 +324,7 @@ uint32_t internal_float_mul(uint32_t b, uint32_t a)
 	/*printf("\e[0;31mPlease implement me at fpu.c\e[0m\n");
 	fflush(stdout);
 	assert(0);*/
-	exp_res = fa.exponent + fb.exponent -127 -23;
+	exp_res = fa.exponent + fb.exponent -127 -20;
 	return internal_normalize(f.sign, exp_res, sig_res);
 }
 
