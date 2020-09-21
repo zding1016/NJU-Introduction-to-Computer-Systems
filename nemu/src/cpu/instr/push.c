5359617727abe1d428858684336a2645d148c2ac
tracer-pa-public <tracer@njuics.org>
2020-09-21 22:10:32 +0800

    > clean > id 191220177 > user pa191220177 > uname Linux 32e3186b035e 4.15.0-45-generic #48~16.04.1-Ubuntu SMP Tue Jan 29 18:03:48 UTC 2019 x86_64 GNU/Linux > uptime 22:10:32 up 14 days, 13:33,  1 user,  load average: 2.31, 2.44, 2.40 > unitime > f7b2b9b67b8010768256ed33918d932d6b11b7bd

--- nemu/src/cpu/instr/push.c
+++ nemu/src/cpu/instr/push.c
@@ -2,3 +2,19 @@
 /*
 Put the implementations of `push' instructions here.
 */
+
+static void instr_execute_1op(){
+    OPERAND dest;
+    oprand_read(&opr_src);
+    dest.type = OPR_MEM;
+    dest.data_size = data_size;
+    cpu.eip = cpu.eip - (data_size) / 8;
+    dest.addr = cpu.eip;
+    oprand_write(&dest);
+}
+
+
+make_instr_impl_1op(push, r, v);
+make_instr_impl_1op(push, rm, v);
+make_instr_impl_1op(push, i, b);
+make_instr_impl_1op(push, i, v);
