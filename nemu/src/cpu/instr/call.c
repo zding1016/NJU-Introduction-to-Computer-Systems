2c643f90a01eee5fcb0549548b893965a048152d
tracer-pa-public <tracer@njuics.org>
2020-09-21 22:43:39 +0800

    > compile nemu testcase kernel > id 191220177 > user pa191220177 > uname Linux 32e3186b035e 4.15.0-45-generic #48~16.04.1-Ubuntu SMP Tue Jan 29 18:03:48 UTC 2019 x86_64 GNU/Linux > uptime 22:43:39 up 14 days, 14:06,  1 user,  load average: 1.92, 2.07, 2.14 > unitime 1600699419158 > bcc8c25f302dfed3d8a81e92eadd475499fbe4a2

--- nemu/src/cpu/instr/call.c
+++ nemu/src/cpu/instr/call.c
@@ -3,5 +3,16 @@
 Put the implementations of `call' instructions here.
 */
 make_instr_func(call){
-    
+    OPERAND src, dest;
+    src.type = OPR_IMM;
+    src.addr = cup.eip + 1;
+    src.data_size = data_size;
+    operand_read(src);
+    cpu.esp = cpu.esp - (data_size / 8);
+    dest.data_size = data_size;
+    dest.type = OPR_MEM;
+    dest.addr = cpu.esp;
+    dest.val = cpu.eip + 1 + data_size / 8;
+    operand_write(&dest);
+    cpu.eip = cpu.eip + src.val + 1 + data_size / 8;
 }
