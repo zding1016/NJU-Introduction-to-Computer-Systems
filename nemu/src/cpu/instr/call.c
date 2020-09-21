d6eaf7fb8f6a1b4190ab8c068700104c392fd11b
tracer-pa-public <tracer@njuics.org>
2020-09-22 00:13:52 +0800

    > compile nemu testcase kernel > id 191220177 > user pa191220177 > uname Linux 32e3186b035e 4.15.0-45-generic #48~16.04.1-Ubuntu SMP Tue Jan 29 18:03:48 UTC 2019 x86_64 GNU/Linux > uptime 00:13:52 up 14 days, 15:36,  1 user,  load average: 2.33, 2.10, 2.07 > unitime 1600704832502 > 864297fe4b9c6b67313756e8534e9bd2fb698e7

--- nemu/src/cpu/instr/call.c
+++ nemu/src/cpu/instr/call.c
@@ -9,7 +9,7 @@ make_instr_func(call){
     src.data_size = data_size;
     operand_read(&src);
     
-    print_asm_2("call","", 1+data_size/8 ,&src);
+    print_asm_1("call","", 1+data_size/8 ,&src);
     int offset = sign_ext(src.val, data_size);
     
     cpu.esp = cpu.esp - (data_size / 8);
