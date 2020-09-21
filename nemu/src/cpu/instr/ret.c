c4ba52c460d2e0dfa4e9bf0d8fbb42b78a2e9aee
tracer-pa-public <tracer@njuics.org>
2020-09-21 23:10:46 +0800

    > clean > id 191220177 > user pa191220177 > uname Linux 32e3186b035e 4.15.0-45-generic #48~16.04.1-Ubuntu SMP Tue Jan 29 18:03:48 UTC 2019 x86_64 GNU/Linux > uptime 23:10:46 up 14 days, 14:33,  1 user,  load average: 2.22, 2.23, 2.20 > unitime > eb8c19aec30b339550ae3f3f4685870b26406b4d

--- nemu/src/cpu/instr/ret.c
+++ nemu/src/cpu/instr/ret.c
@@ -2,3 +2,13 @@
 /*
 Put the implementations of `ret' instructions here.
 */
+make_instr_func(ret){
+    OPERAND src;
+    src.type = OPR_MEM;
+    src.data_size = data_size;
+    src.addr = cpu.esp;
+    operand_read(&src);
+    cpu.eip = src.val;
+    cpu.esp = cpu.esp - (data_size / 8);
+    
+}
