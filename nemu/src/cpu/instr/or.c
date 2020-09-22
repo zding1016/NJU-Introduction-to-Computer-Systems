04979266cf3aead666bb3ab5cbf53359bdcaa288
tracer-pa-public <tracer@njuics.org>
2020-09-22 18:05:49 +0800

    > compile nemu testcase kernel > id 191220177 > user pa191220177 > uname Linux 32e3186b035e 4.15.0-45-generic #48~16.04.1-Ubuntu SMP Tue Jan 29 18:03:48 UTC 2019 x86_64 GNU/Linux > uptime 18:05:49 up 15 days,  9:28,  2 users,  load average: 2.11, 2.10, 2.15 > unitime 1600769149193 > daea4d576aaab575df2bbca7f3e905d862fcc327

--- nemu/src/cpu/instr/or.c
+++ nemu/src/cpu/instr/or.c
@@ -5,7 +5,7 @@ Put the implementations of `or' instructions here.
 static void instr_execute_2op(){
     operand_read(&opr_src);
     operand_read(&opr_dest);
-    opr_dest.val = alu_or(opr_src, opr_dest, data_size);
+    opr_dest.val = alu_or(opr_src.val, opr_dest.val, data_size);
     operand_write(&opr_dest);
 }
 
