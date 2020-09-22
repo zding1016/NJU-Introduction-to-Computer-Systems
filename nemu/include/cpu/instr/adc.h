62c642138d9aea6804c7e75d74ebbed387ca05fb
tracer-pa-public <tracer@njuics.org>
2020-09-22 19:11:44 +0800

    > compile nemu testcase kernel > id 191220177 > user pa191220177 > uname Linux 32e3186b035e 4.15.0-45-generic #48~16.04.1-Ubuntu SMP Tue Jan 29 18:03:48 UTC 2019 x86_64 GNU/Linux > uptime 19:11:44 up 15 days, 10:34,  4 users,  load average: 2.09, 2.14, 2.24 > unitime 1600773104166 > 877e7bfa5ff8f8f6775e2b48e72c984c2c18f133

--- nemu/include/cpu/instr/adc.h
+++ nemu/include/cpu/instr/adc.h
@@ -4,4 +4,9 @@
 Put the declarations of `adc' instructions here.
 */
 make_instr_func(adc_r2rm_v);
+make_instr_func(adc_r2rm_b);
+make_instr_func(adc_rm2r_v);
+make_instr_func(adc_rm2r_b);
+make_instr_func(adc_i2a_v);
+make_instr_func(adc_i2a_b);
 #endif
