5fc41ede3d9a754356a2b147ab3ffe7787aabd3a
tracer-pa-public <tracer@njuics.org>
2020-09-22 09:34:04 +0800

    > compile nemu testcase kernel > id 191220177 > user pa191220177 > uname Linux 32e3186b035e 4.15.0-45-generic #48~16.04.1-Ubuntu SMP Tue Jan 29 18:03:48 UTC 2019 x86_64 GNU/Linux > uptime 09:34:04 up 15 days, 56 min,  2 users,  load average: 2.13, 2.23, 2.26 > unitime 1600738444279 > 965c9ee5890a93516db11e649be44009df4a4ba

--- nemu/src/cpu/instr/opcode_2_byte.c
+++ nemu/src/cpu/instr/opcode_2_byte.c
@@ -4,6 +4,7 @@ make_instr_func(opcode_2_byte)
 {
 	int len = 1;
 	uint8_t op = instr_fetch(eip + 1, 1);
+	printf("%x",op);
 #ifdef NEMU_REF_INSTR
 	len += __ref_opcode_2_byte_entry[op](eip + 1, op);
 #else
