c765c0f5a29a7ca03533e0b30458f3614a0daead
tracer-pa-public <tracer@njuics.org>
2020-09-22 10:26:54 +0800

    > compile nemu testcase kernel > id 191220177 > user pa191220177 > uname Linux 32e3186b035e 4.15.0-45-generic #48~16.04.1-Ubuntu SMP Tue Jan 29 18:03:48 UTC 2019 x86_64 GNU/Linux > uptime 10:26:54 up 15 days,  1:49,  4 users,  load average: 3.51, 2.56, 2.36 > unitime 1600741614761 > 5b2b2ef5919cf9935bc2ba44408212c8df9eb57e

--- nemu/src/cpu/instr/pop.c
+++ nemu/src/cpu/instr/pop.c
@@ -3,14 +3,14 @@
 Put the implementations of `pop' instructions here.
 */
 static void instr_execute_1op(){
-    opr_src.data_size = data_size;
-    opr_src.type = OPR_MEM;
-    opr_src.addr = cpu.esp;
-    operand_read(&opr_src);
+    opr_dest.data_size = data_size;
+    opr_dest.type = OPR_MEM;
+    opr_dest.addr = cpu.esp;
+    operand_read(&opr_dest);
     
     cpu.esp += (data_size / 8);
-    opr_dest.val = opr_src.val;
-    operand_write(&opr_dest);
+    opr_src.val = opr_dest.val;
+    operand_write(&opr_src);
 }
 
 make_instr_impl_1op(pop, r, v)
