027a8b0e64d79a9e4ec765825ad3a03560a3c8f8
tracer-pa-public <tracer@njuics.org>
2020-09-22 18:02:20 +0800

    > compile nemu testcase kernel > id 191220177 > user pa191220177 > uname Linux 32e3186b035e 4.15.0-45-generic #48~16.04.1-Ubuntu SMP Tue Jan 29 18:03:48 UTC 2019 x86_64 GNU/Linux > uptime 18:02:20 up 15 days,  9:25,  2 users,  load average: 2.08, 2.07, 2.16 > unitime 1600768940178 > 2500b1a93fa087ab1232bafab0be41d91ea8720

--- nemu/src/cpu/instr/call.c
+++ nemu/src/cpu/instr/call.c
@@ -2,7 +2,7 @@
 /*
 Put the implementations of `call' instructions here.
 */
-/*make_instr_func(call_near){
+make_instr_func(call_near){
     OPERAND src;
     src.type = OPR_IMM;
     src.addr = eip + 1;
@@ -15,7 +15,7 @@ Put the implementations of `call' instructions here.
     cpu.esp = cpu.esp - (data_size / 8);
     opr_dest.data_size = data_size;
     opr_dest.type = OPR_MEM;
-    cpu.esp = cpu.esp - (32 / 8);
+    
     opr_dest.addr = cpu.esp;
     eip = eip + 1 + data_size / 8;
     opr_dest.val = eip;
@@ -23,23 +23,4 @@ Put the implementations of `call' instructions here.
     operand_write(&opr_dest);
     cpu.eip = eip + offset;
     return 0;
-}*/
-make_instr_func(call_near) {
-	OPERAND rel,mem;
-	rel.data_size=data_size;
-    rel.type=OPR_IMM;
-	//rel.sreg=SREG_CS;
-    rel.addr=cpu.eip+1;
-    operand_read(&rel);
-	print_asm_1("call", "", 1+data_size/8, &rel);
-	cpu.esp-=data_size/8;
-	mem.data_size=data_size;
-	mem.type=OPR_MEM;
-	//mem.sreg=SREG_DS;
-	mem.addr=cpu.esp;
-	mem.val=cpu.eip+1+data_size/8;
-	operand_write(&mem);
-	int offset=sign_ext(rel.val, data_size);
-	cpu.eip+=offset+1+data_size/8;
-	return 0;
 }
