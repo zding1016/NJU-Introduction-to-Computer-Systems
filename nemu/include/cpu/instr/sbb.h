02a7264d40d716bfd6d392481c2c0311b72d72b6
tracer-pa-public <tracer@njuics.org>
2020-09-22 19:57:24 +0800

    > compile nemu testcase kernel > id 191220177 > user pa191220177 > uname Linux 32e3186b035e 4.15.0-45-generic #48~16.04.1-Ubuntu SMP Tue Jan 29 18:03:48 UTC 2019 x86_64 GNU/Linux > uptime 19:57:24 up 15 days, 11:20,  4 users,  load average: 2.25, 2.48, 2.51 > unitime 1600775844149 > 926d245e7f88fd78f19e619d8547e31c681e0f34

--- nemu/include/cpu/instr/sbb.h
+++ nemu/include/cpu/instr/sbb.h
@@ -9,4 +9,6 @@ make_instr_func(sbb_rm2r_b);
 make_instr_func(sbb_rm2r_v);
 make_instr_func(sbb_i2a_b);
 make_instr_func(sbb_i2a_v);
+make_instr_func(sbb_i2rm_b);
+make_instr_func(sbb_i2rm_v);
 #endif
