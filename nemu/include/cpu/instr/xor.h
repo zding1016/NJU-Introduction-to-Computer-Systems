af218230cd9e7ebae51830efc5805be01a3f377b
tracer-pa-public <tracer@njuics.org>
2020-09-22 19:28:50 +0800

    > compile nemu testcase kernel > id 191220177 > user pa191220177 > uname Linux 32e3186b035e 4.15.0-45-generic #48~16.04.1-Ubuntu SMP Tue Jan 29 18:03:48 UTC 2019 x86_64 GNU/Linux > uptime 19:28:50 up 15 days, 10:51,  4 users,  load average: 2.37, 2.31, 2.26 > unitime 1600774129800 > 1a37311969bd875bd8ba9bca11d881147db7ff9

--- nemu/include/cpu/instr/xor.h
+++ nemu/include/cpu/instr/xor.h
@@ -4,4 +4,9 @@
 Put the declarations of `xor' instructions here.
 */
 make_instr_func(xor_r2rm_v);
+make_instr_func(xor_r2rm_b);
+make_instr_func(xor_rm2r_v);
+make_instr_func(xor_rm2r_b);
+make_instr_func(xor_i2a_v);
+make_instr_func(xor_i2a_b);
 #endif
--- nemu/src/cpu/instr/xor.c
+++ nemu/src/cpu/instr/xor.c
@@ -10,3 +10,8 @@ static void instr_execute_2op(){
 }
 
 make_instr_impl_2op(xor, r, rm, v)
+make_instr_impl_2op(xor, r, rm, b)
+make_instr_impl_2op(xor, rm, r, v)
+make_instr_impl_2op(xor, rm, r, b)
+make_instr_impl_2op(xor, i, a, v)
+make_instr_impl_2op(xor, i, a, b)
