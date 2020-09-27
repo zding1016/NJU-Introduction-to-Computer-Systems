87a211ecb45ec828bf9cec25ec6e080eb3445f8e
tracer-pa-public <tracer@njuics.org>
2020-09-27 08:27:28 +0800

    > clean > id 191220177 > user pa191220177 > uname Linux 32e3186b035e 4.15.0-45-generic #48~16.04.1-Ubuntu SMP Tue Jan 29 18:03:48 UTC 2019 x86_64 GNU/Linux > uptime 08:27:28 up 19 days, 23:50,  1 user,  load average: 2.39, 2.24, 2.19 > unitime > a140e5bedbdc644657c95ffb99a9de983a64ec57

--- nemu/src/cpu/instr/ret.c
+++ nemu/src/cpu/instr/ret.c
@@ -5,12 +5,12 @@ Put the implementations of `ret' instructions here.
 make_instr_func(ret_near){
     OPERAND src;
     src.type = OPR_MEM;
-    src.data_size = data_size;
+    src.data_size = 32;
     src.addr = cpu.esp;
     operand_read(&src);
     print_asm_0("ret","",1);
-    cpu.esp += data_size / 8;
-    cpu.eip = sign_ext(src.val,src.data_size);
+    cpu.esp += 32 / 8;
+    cpu.eip = src.val;
     return 0;
 }
 
@@ -18,11 +18,11 @@ make_instr_func(ret_near_imm16){
     print_asm_0("ret","",3);
     OPERAND mem;
     mem.type = OPR_MEM;
-    mem.data_size = data_size;
+    mem.data_size = 32;
     mem.addr = cpu.esp;
     operand_read(&mem);
     cpu.eip = mem.val;
     int offset = sign_ext(instr_fetch(eip + 1, 2), 16);
-    cpu.esp += offset + data_size / 8;
+    cpu.esp += offset + 16 / 8;
     return 0;
 }
