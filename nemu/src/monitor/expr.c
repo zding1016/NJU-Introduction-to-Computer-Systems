5c3c4e4c2eb890b1d2a6e4861f0d5c260f616add
tracer-pa-public <tracer@njuics.org>
2020-09-29 16:51:03 +0800

    > compile nemu testcase kernel > id 191220177 > user pa191220177 > uname Linux 32e3186b035e 4.15.0-45-generic #48~16.04.1-Ubuntu SMP Tue Jan 29 18:03:48 UTC 2019 x86_64 GNU/Linux > uptime 16:51:03 up 22 days,  8:13,  1 user,  load average: 2.01, 2.21, 2.22 > unitime 1601369463545 > a13615ca36bb55ad9444419b9bccac6c49780cbe

--- nemu/src/monitor/expr.c
+++ nemu/src/monitor/expr.c
@@ -209,6 +209,7 @@ uint32_t eval(int s, int e, bool *success)
                 }
             }
         }
+        uint32_t val1, val2;
         val1 = eval(p, op - 1, success);
         val2 = eval(op + 1, q, success);
         switch(op_type) {
