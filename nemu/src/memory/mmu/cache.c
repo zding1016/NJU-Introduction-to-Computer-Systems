b78b1ce269f925ecb4beced770b2dcdc938e363d
tracer-pa-public <tracer@njuics.org>
2020-10-01 20:56:38 +0800

    > compile nemu testcase kernel > id 191220177 > user pa191220177 > uname Linux 32e3186b035e 4.15.0-45-generic #48~16.04.1-Ubuntu SMP Tue Jan 29 18:03:48 UTC 2019 x86_64 GNU/Linux > uptime 20:56:38 up 24 days, 12:19,  1 user,  load average: 2.03, 2.09, 2.08 > unitime 1601556998335 > 9b1c7b259d647aceb128eb72caa497f53d257c76

--- nemu/src/memory/mmu/cache.c
+++ nemu/src/memory/mmu/cache.c
@@ -1,9 +1,18 @@
 #include "memory/mmu/cache.h"
 
+struct Cache_Line{
+    uint8_t valid_byte:1;
+    uint32_t sign_byte:19;
+    uint8_t block[64];
+}
+
+struct Cache_Line Cache[1024];
 // init the cache
 void init_cache()
 {
 	// implement me in PA 3-1
+	for (int i = 0; i < 1024; i++)
+	    Cache[i].valid_byte = 0;
 }
 
 // write data to cache
