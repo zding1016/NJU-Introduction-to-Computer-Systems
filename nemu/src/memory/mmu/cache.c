a18d05e78de299c81e6e42046ff93214978043e5
tracer-pa-public <tracer@njuics.org>
2020-10-01 21:34:14 +0800

    > clean > id 191220177 > user pa191220177 > uname Linux 32e3186b035e 4.15.0-45-generic #48~16.04.1-Ubuntu SMP Tue Jan 29 18:03:48 UTC 2019 x86_64 GNU/Linux > uptime 21:34:14 up 24 days, 12:57,  1 user,  load average: 2.27, 2.28, 2.21 > unitime > f680e22298d8a4fa1845a149ec240f34e9aa196f

--- nemu/src/memory/mmu/cache.c
+++ nemu/src/memory/mmu/cache.c
@@ -1,12 +1,13 @@
 #include "memory/mmu/cache.h"
 
 struct Cache_Line{
-    uint8_t valid_byte:1;
-    uint32_t sign_byte:19;
-    uint8_t block[64];
+    uint8_t valid_byte:1; //有效位 1位
+    uint32_t sign_byte:19; //标记位 32 - 6 - 7 = 19位 
+    uint8_t block[64]; // 2^6 位
 };
 
-struct Cache_Line Cache[1024];
+struct Cache_Line Cache[1024]; //共有1024行
+//但是我们要实现8行一组，所以共有2^7组，组号的位数为7
 // init the cache
 void init_cache()
 {
@@ -19,12 +20,91 @@ void init_cache()
 void cache_write(paddr_t paddr, size_t len, uint32_t data)
 {
 	// implement me in PA 3-1
+	uint32_t sign_byte = (paddr >> 13) & 0x7ffff;
+	uint32_t index_for_group = (paddr >> 6) & 0x7f;
+	uint32_t index_in_group = paddr & 0x3f;
+	bool whether_hit = false;
+	uint32_t index_empty_in_group = -1;
+	
+	memcpy(hw_mem + paddr, &data, len);
+	for (int i = 0; i< 8; i++){
+	    uint32_t index_line = index_for_group *8 + i;
+	    if (Cache[index_line].valid_byte == 1){
+	        if (Cache[index_line].sign_byte == sign_byte) {
+	            whether_hit = true;
+	            for (int j = 0; j< len; j++){
+	                if (index_in_group +j > 64) {
+	                    cache_write(paddr + j, len-j, data);
+	                    return;
+	                }
+	                Cache[index_line].block[index_in_group +j] =data &0xff;
+	                data = data>>8;
+	            }
+	            return;
+	        }
+	    }else {
+	        index_empty_in_group = index_line;
+	    }
+	}
+	if (!whether_hit){
+	    if (index_empty_in_group == -1){
+	        index_empty_in_group = index_for_group + (rand() % 8);
+	    }
+	    uint32_t start_addr = paddr & 0xffffffc0;
+	    memcpy(Cache[index_empty_in_group].block, hw_mem +start_addr , 64);
+	    Cache[index_empty_in_group].valid_byte =1;
+	    Cache[index_empty_in_group].sign_byte =sign_byte;
+	}
 }
 
 // read data from cache
 uint32_t cache_read(paddr_t paddr, size_t len)
 {
 	// implement me in PA 3-1
-	return 0;
+	uint32_t sign_byte = (paddr >> 13) & 0x7ffff;
+	uint32_t index_for_group = (paddr >> 6) & 0x7f;
+	uint32_t index_in_group = paddr & 0x3f;
+	
+	bool whether_hit = false;
+	uint32_t index_empty_in_group = -1;
+	uint32_t ans = 0;
+	for (int i = 0; i < 8; i++){
+	    uint32_t index_line = index_for_group * 8 + i;
+	    if (Cache[index_line].valid_byte == 1) { //有效位有效
+	        if (Cache[index_line].sign_byte == sign_byte){ //tmd命中了
+	            whether_hit = true;
+	            for (int j = 0; j < len; j++){ //开始一位位的读取
+	                if (index_in_group + j >= 64) { //qnmd跨行了
+	                    uint32_t ans_rest = cache_read(paddr + j, len - j);
+	                    ans = ans | (ans_rest << (8*j));
+	                    return ans;
+	                }
+	                else {
+	                    ans = ans | (Cache[index_line].block[index_in_group + j] << (j*8));
+	                }
+	            }
+	            return ans;
+	        }
+	    } else {
+	        index_empty_in_group = index_line;
+	    }
+	}
+	
+	if (!whether_hit) {
+	    memcpy(&ans, hw_mem + paddr, len);
+	    uint32_t start_addr = paddr & 0xffffffc0;
+	    if (index_empty_in_group != -1){
+	        memcpy(Cache[index_empty_in_group].block, hw_mem + start_addr, 64);
+	        Cache[index_empty_in_group].valid_byte = 1;
+	        Cache[index_empty_in_group].sign_byte = sign_byte;
+	    }
+	    else{
+	        int random_line = (rand() % 8) + index_for_group * 8;
+	        memcpy(Cache[random_line].block, hw_mem + start_addr, 64);
+	        Cache[random_line].valid_byte = 1;
+	        Cache[random_line].sign_byte = sign_byte;
+	    }
+	}
+	return ans;
 }
 
