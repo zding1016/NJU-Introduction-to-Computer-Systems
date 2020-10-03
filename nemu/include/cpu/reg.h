7e70a7ed4abdc0e3c94c78273f958f8a2fa4f0a2
tracer-pa-public <tracer@njuics.org>
2020-10-03 16:08:59 +0800

    > clean > id 191220177 > user pa191220177 > uname Linux 32e3186b035e 4.15.0-45-generic #48~16.04.1-Ubuntu SMP Tue Jan 29 18:03:48 UTC 2019 x86_64 GNU/Linux > uptime 16:08:59 up 26 days,  7:31,  1 user,  load average: 2.13, 2.25, 2.25 > unitime > 10bcbfe5ed5c48d769b316813b6fb03fd9482d85

--- nemu/include/cpu/reg.h
+++ nemu/include/cpu/reg.h
@@ -56,12 +56,12 @@ typedef struct
 
 #ifdef IA32_SEG
 
-    typedef struct GDTR{
+    struct GDTR{
         uint32_t limit : 16;
         uint32_t base : 32;
-    }GDTR;
+    };
     
-    typedef union CR0{
+    union CR0{
         struct {
             uint32_t pe : 1;
             uint32_t mp : 1;
@@ -72,9 +72,9 @@ typedef struct
             uint32_t pg : 1;
         };
         uint32_t val;
-    }CR0;
+    };
     
-    typedef struct SegReg{
+    struct SegReg{
         // the 16-bit visible part, i.e. , the selector
         union {
             uint16_t val;
@@ -93,15 +93,15 @@ typedef struct
             uint32_t privilege_level : 2;
             uint32_t soft_use : 1;
         };
-    }SegReg;
+    };
     
-	GDTR gdtr; // GDTR, todo: define type GDTR
+	struct GDTR gdtr; // GDTR, todo: define type GDTR
 	// segment registers, todo: define type SegReg
 	union {
-		SegReg segReg[6];
+		struct SegReg segReg[6];
 		struct
 		{
-			SegReg es, cs, ss, ds, fs, gs;
+			struct SegReg es, cs, ss, ds, fs, gs;
 		};
 	};
 	// control registers, todo: define type CR0
