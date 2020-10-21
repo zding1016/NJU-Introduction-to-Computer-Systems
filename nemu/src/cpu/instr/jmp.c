f2285f66c29eb71223a6d829d68b3f768d5a3982
tracer-pa-public <tracer@njuics.org>
2020-10-21 14:29:35 +0800

    > compile nemu testcase kernel > id 191220177 > user pa191220177 > uname Linux 32e3186b035e 4.15.0-45-generic #48~16.04.1-Ubuntu SMP Tue Jan 29 18:03:48 UTC 2019 x86_64 GNU/Linux > uptime 14:29:35 up 44 days,  5:52,  1 user,  load average: 2.39, 2.35, 2.31 > unitime 1603261775528 > 88d3640bd9a31a0f4073515d9358569656860a7

--- nemu/src/cpu/instr/jmp.c
+++ nemu/src/cpu/instr/jmp.c
@@ -52,16 +52,18 @@ make_instr_func(jmp_short){
 }
 
 make_instr_func(jmp_far_imm){
-    OPERAND ptr16, ptr32;
-    ptr16.type = ptr32.type = OPR_IMM;
-    ptr16.data_size = 16;
-    ptr32.data_size = 32;
-    ptr16.addr = eip + 5;
-    ptr32.addr = eip + 1;
-    operand_read(&ptr16);
-    operand_read(&ptr32);
-    cpu.cs.val = ptr16.val;
-    cpu.eip = ptr32.val;
-    load_sreg(1);
+    OPERAND rel;
+	rel.type = OPR_IMM;
+	rel.sreg = SREG_CS;
+    rel.data_size = 32;
+    rel.addr = eip + 1;
+	operand_read(&rel);
+	print_asm_1("jmp", "", 7, &rel);
+	if(data_size == 16)
+		cpu.eip = rel.val & 0xFFFF;
+	else
+		cpu.eip = rel.val;
+	cpu.cs.val = instr_fetch(eip + 5, 2);
+	load_sreg(1);
     return 0;
 }
