65dbf08025103c96b53c25aa1ac5358250f7574c
tracer-pa-public <tracer@njuics.org>
2020-10-21 14:23:17 +0800

    > compile nemu testcase kernel > id 191220177 > user pa191220177 > uname Linux 32e3186b035e 4.15.0-45-generic #48~16.04.1-Ubuntu SMP Tue Jan 29 18:03:48 UTC 2019 x86_64 GNU/Linux > uptime 14:23:17 up 44 days,  5:46,  1 user,  load average: 2.12, 2.21, 2.26 > unitime 1603261397790 > 95a9b543748816b12c659b1cba4ebdc7c84a5579

--- nemu/src/cpu/instr/mov.c
+++ nemu/src/cpu/instr/mov.c
@@ -78,36 +78,41 @@ make_instr_func(mov_srm162r_l) {
 }
 
 make_instr_func(mov_rm2s_w) {
-    OPERAND rm, s;
-    rm.data_size = s.data_size = 16;
-    modrm_rm_s(eip + 1, &rm, &s);
-    operand_read(&rm);
-    s.val = rm.val;
-    operand_write(&s);
-    load_sreg(s.addr);
-    print_asm_2("mov", "w", 2, &opr_src, &opr_dest);
-    return 2;
+    int len = 1;
+    decode_data_size_w
+    decode_operand_rm2r
+    operand_read(&opr_src);
+    opr_dest.val = opr_src.val;
+    opr_dest.type = OPR_SREG;
+    operand_write(&opr_dest);
+    load_sreg(opr_dest.addr);
+    print_asm_2("mov", "w", len, &opr_src, &opr_dest);
+    return len;
 }
 
 make_instr_func(mov_c2r_l){
+    int len = 1;
     decode_data_size_l
-    modrm_c_r(eip + 1, &opr_dest, &opr_src);
+    decode_operand_r2rm
+    opr_src.type = OPR_CREG;
     operand_read(&opr_src);
     opr_dest.val = opr_src.val;
     operand_write(&opr_dest);
-    print_asm_2("mov", opr_dest.data_size == 8 ? "b" : (opr_dest.data_size == 16 ? "w" : "l"), 2, &opr_src, &opr_dest);
-    return 2;
+    print_asm_2("mov", opr_dest.data_size == 8 ? "b" : (opr_dest.data_size == 16 ? "w" : "l"), len, &opr_src, &opr_dest);
+    return len;
     
 }
 
 make_instr_func(mov_r2c_l){
+    int len = 1;
     decode_data_size_l
-    modrm_c_r(eip + 1, &opr_src, &opr_dest);
+    decode_operand_rm2r
     operand_read(&opr_src);
+    opr_dest.type = OPR_CREG;
     opr_dest.val = opr_src.val;
     operand_write(&opr_dest);
-    print_asm_2("mov", opr_dest.data_size == 8 ? "b" : (opr_dest.data_size == 16 ? "w" : "l"), 2, &opr_dest, &opr_src);
-    return 2;
+    print_asm_2("mov", opr_dest.data_size == 8 ? "b" : (opr_dest.data_size == 16 ? "w" : "l"), len, &opr_dest, &opr_src);
+    return len;
     
 }
 
