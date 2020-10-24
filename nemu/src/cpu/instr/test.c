a4445d8f05e13b1b7725c673504c03bdd88b65bd
tracer-pa-public <tracer@njuics.org>
2020-10-24 16:19:12 +0800

    > compile nemu testcase kernel > id 191220177 > user pa191220177 > uname Linux 32e3186b035e 4.15.0-45-generic #48~16.04.1-Ubuntu SMP Tue Jan 29 18:03:48 UTC 2019 x86_64 GNU/Linux > uptime 16:19:12 up 47 days,  7:41,  1 user,  load average: 2.58, 2.30, 2.21 > unitime 1603527552137 > 8fdf3c822efb933dd88a7fda788b4f05207c57e1

--- nemu/src/cpu/instr/test.c
+++ nemu/src/cpu/instr/test.c
@@ -7,7 +7,7 @@ static void instr_execute_2op(){
     operand_read(&opr_dest);
     opr_src.val = sign_ext(opr_src.val, data_size);
     opr_dest.val = sign_ext(opr_dest.val, data_size);
-    alu_and(opr_src.val,opr_dest.val,data_size);
+    alu_and(opr_src.val,opr_dest.val,opr_dest.data_size);
 }
 
 make_instr_impl_2op(test, r, rm, v)
