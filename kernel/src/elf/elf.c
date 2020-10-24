c5e3178169e58b03e5d6588adce126381660d094
tracer-pa-public <tracer@njuics.org>
2020-10-24 16:26:00 +0800

    > compile nemu testcase kernel > id 191220177 > user pa191220177 > uname Linux 32e3186b035e 4.15.0-45-generic #48~16.04.1-Ubuntu SMP Tue Jan 29 18:03:48 UTC 2019 x86_64 GNU/Linux > uptime 16:26:00 up 47 days,  7:48,  1 user,  load average: 2.08, 2.17, 2.19 > unitime 1603527960119 > 7d10361b55f9b568c14225331e6032eca9be97

--- kernel/src/elf/elf.c
+++ kernel/src/elf/elf.c
@@ -55,7 +55,7 @@ uint32_t loader()
             //ide_read((uint8_t *)mem, ph->p_offset, ph->p_filesz);
 #endif
 
-uint32_t paddr=mm_malloc(ph->p_vaddr,ph->p_memsz);
+    mm_malloc(ph->p_vaddr,ph->p_memsz);
 			//ide_read((uint8_t *)paddr,ph->p_offset,ph->p_filesz);
 #ifdef IA32_PAGE
 			/* Record the program break for future use */
