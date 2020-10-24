33bb36ca363cce015299ca48cee20854d7bdf159
tracer-pa-public <tracer@njuics.org>
2020-10-24 19:50:48 +0800

    > compile nemu testcase kernel > id 191220177 > user pa191220177 > uname Linux 32e3186b035e 4.15.0-45-generic #48~16.04.1-Ubuntu SMP Tue Jan 29 18:03:48 UTC 2019 x86_64 GNU/Linux > uptime 19:50:48 up 47 days, 11:13,  2 users,  load average: 3.00, 3.00, 2.67 > unitime 1603540247905 > 283063975ad59a1f5ce377e66b20c71106e8a6b

--- nemu/src/cpu/decode/opcode.c
+++ nemu/src/cpu/decode/opcode.c
@@ -85,23 +85,23 @@ instr_func group_2_b_entry[8] =
 
 /* 0xc1 */
 instr_func group_2_v_entry[8] =
-    {inv, inv, inv, inv, __ref_shl_i2rm_bv, __ref_shr_i2rm_bv, inv, __ref_sar_i2rm_bv};
+    {inv, inv, inv, inv, shl_i2rm_bv, shr_i2rm_bv, inv, sar_i2rm_bv};
 
 /* 0xd0 */
 instr_func group_2_1b_entry[8] =
-    {inv, inv, inv, inv, __ref_shl_rm_b, __ref_shr_rm_b, inv, __ref_sar_rm_b};
+    {inv, inv, inv, inv, shl_rm_b, shr_rm_b, inv, sar_rm_b};
 
 /* 0xd1 */
 instr_func group_2_1v_entry[8] =
-    {inv, inv, inv, inv, __ref_shl_rm_v, __ref_shr_rm_v, inv, __ref_sar_rm_v};
+    {inv, inv, inv, inv, shl_rm_v, shr_rm_v, inv, sar_rm_v};
 
 /* 0xd2 */
 instr_func group_2_cb_entry[8] =
-    {inv, inv, inv, inv, __ref_shl_c2rm_b, __ref_shr_c2rm_b, inv, __ref_sar_c2rm_b};
+    {inv, inv, inv, inv, shl_c2rm_b, shr_c2rm_b, inv, sar_c2rm_b};
 
 /* 0xd3 */
 instr_func group_2_cv_entry[8] =
-    {inv, inv, inv, inv, __ref_shl_c2rm_bv, __ref_shr_c2rm_bv, inv, __ref_sar_c2rm_bv};
+    {inv, inv, inv, inv, shl_c2rm_bv, shr_c2rm_bv, inv, sar_c2rm_bv};
 
 /* 0xf6 */
 instr_func group_3_b_entry[8] =
