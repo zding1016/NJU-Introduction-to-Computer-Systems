6995e8de48ff8c28f6fb8e2ab1bb2b2865784db8
tracer-pa-public <tracer@njuics.org>
2020-12-14 14:52:01 +0800

    > clean > id 191220177 > user pa191220177 > uname Linux 32e3186b035e 4.15.0-45-generic #48~16.04.1-Ubuntu SMP Tue Jan 29 18:03:48 UTC 2019 x86_64 GNU/Linux > uptime 14:52:01 up 98 days,  6:14,  2 users,  load average: 2.40, 2.31, 2.28 > unitime > 3d0c52399824d469a95ec82027c70d8d15006775

--- nemu/src/memory/mmu/page.c
+++ nemu/src/memory/mmu/page.c
@@ -5,10 +5,10 @@
 paddr_t page_translate(laddr_t laddr)
 {
 #ifndef TLB_ENABLED
-	uint32_t dir=laddr>>22;
+    uint32_t dir=laddr>>22;
 	uint32_t page=(laddr>>12)&(0xffffffff>>22);
 	uint32_t offset=laddr&(0xffffffff>>20);
-	printf("%x %x %x %x",laddr,dir,page,offset);
+	//fprintf(stderr,"%x %x %x %x",laddr,dir,page,offset);
 	//page_directory
     uint32_t page_directory_index=cpu.cr3.page_directory_base<<12;
     PDE page_directory;
@@ -18,13 +18,13 @@ paddr_t page_translate(laddr_t laddr)
     //page_table
     uint32_t page_table_index=page_directory.page_frame<<12;
     PTE page_table;
-    fprintf(stderr,"pageTable_addr is %x\n",page_table_index);
-	fprintf(stderr,"addr is %x\n",page_table_index+page*4);
+    //fprintf(stderr,"pageTable_addr is %x\n",page_table_index);
+	//fprintf(stderr,"addr is %x\n",page_table_index+page*4);
     memcpy(&page_table,hw_mem+page_table_index+page*4,sizeof(page_table));
      
     assert(page_table.present==1);
     //page
-   assert(0);
+   
     return (page_table.page_frame<<12)+offset;
 	
 #else
