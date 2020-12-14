c804d3d099aa4a55fd3f831c4900b20b63b3a4cb
tracer-pa-public <tracer@njuics.org>
2020-12-14 15:25:54 +0800

    > clean > id 191220177 > user pa191220177 > uname Linux 32e3186b035e 4.15.0-45-generic #48~16.04.1-Ubuntu SMP Tue Jan 29 18:03:48 UTC 2019 x86_64 GNU/Linux > uptime 15:25:54 up 98 days,  6:48,  1 user,  load average: 2.49, 2.51, 2.47 > unitime > 1f99ce0bd12e7fc79b519fa58176fbc53462635

--- nemu/src/cpu/instr/pop.c
+++ nemu/src/cpu/instr/pop.c
@@ -19,3 +19,26 @@ make_instr_impl_1op(pop, r, v)
 make_instr_impl_1op(pop, rm, v)
 make_instr_impl_1op(pop, i, b)
 make_instr_impl_1op(pop, i, v)
+
+make_instr_func(popa){
+	OPERAND reg[8];
+	int len = 1;
+
+	for(int i=0;i<8;i++){
+		reg[i].data_size = data_size;
+		reg[i].type = OPR_MEM;
+		reg[i].sreg = SREG_SS;
+	}
+	for(int i=7;i>=0;i--){
+		reg[i].addr = cpu.esp;
+		cpu.esp += data_size/8;
+		operand_read(&reg[i]);
+	}
+	for(int i=0;i<8;i++){
+		reg[i].type = OPR_REG;
+		reg[i].addr = i;
+		if(i!=REG_ESP)
+			operand_write(&reg[i]);
+	}
+	return len;
+}
--- nemu/src/cpu/instr/push.c
+++ nemu/src/cpu/instr/push.c
@@ -20,3 +20,23 @@ make_instr_impl_1op(push, r, v)
 make_instr_impl_1op(push, rm, v)
 make_instr_impl_1op(push, i, b)
 make_instr_impl_1op(push, i, v)
+
+make_instr_func(pusha){
+	uint32_t t=cpu.esp;
+	for(int i=0;i<8;i++){
+		OPERAND rel;
+		rel.data_size=data_size;
+		rel.type=OPR_MEM;
+		rel.sreg=SREG_SS;
+		cpu.esp-=4;
+		rel.addr=cpu.esp;
+		if(i==4){
+			rel.val=t;
+		}
+		else{
+			rel.val=cpu.gpr[i].val;
+		}
+		operand_write(&rel);
+	}
+	return 1;
+}
