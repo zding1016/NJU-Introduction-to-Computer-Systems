be28fe78e488675d81e44016ba6d2e1dc3f3630d
tracer-pa-public <tracer@njuics.org>
2020-12-14 15:22:00 +0800

    > clean > id 191220177 > user pa191220177 > uname Linux 32e3186b035e 4.15.0-45-generic #48~16.04.1-Ubuntu SMP Tue Jan 29 18:03:48 UTC 2019 x86_64 GNU/Linux > uptime 15:22:00 up 98 days,  6:44,  1 user,  load average: 2.12, 2.48, 2.46 > unitime > fce8fa4e132e3418a46cbc1fab89e1781c4f9663

--- nemu/src/cpu/instr/lidt.c
+++ nemu/src/cpu/instr/lidt.c
@@ -2,3 +2,18 @@
 /*
 Put the implementations of `lidt' instructions here.
 */
+make_instr_func(lidt)
+{
+	OPERAND t;
+	t.data_size = data_size;
+	int len = 1;
+	len=len + modrm_rm(eip + 1, &t);
+	cpu.idtr.limit=laddr_read(t.addr,2);
+	if(data_size==32){
+		cpu.idtr.base=laddr_read(t.addr+2,4);
+	}
+	else{
+		cpu.idtr.base=laddr_read(t.addr+2,3);
+	}
+	return len;
+}
