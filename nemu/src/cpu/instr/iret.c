8862e1c954530662af6bbb76b49a17cb254c7062
tracer-pa-public <tracer@njuics.org>
2020-12-14 15:27:27 +0800

    > compile nemu testcase kernel > id 191220177 > user pa191220177 > uname Linux 32e3186b035e 4.15.0-45-generic #48~16.04.1-Ubuntu SMP Tue Jan 29 18:03:48 UTC 2019 x86_64 GNU/Linux > uptime 15:27:27 up 98 days,  6:50,  1 user,  load average: 2.61, 2.58, 2.50 > unitime 1607930847067 > 7449ab82c854f74213c87e9f37eb857a7a06eaa8

--- nemu/src/cpu/instr/iret.c
+++ nemu/src/cpu/instr/iret.c
@@ -2,3 +2,13 @@
 /*
 Put the implementations of `iret' instructions here.
 */
+make_instr_func(iret)
+{
+	cpu.eip=vaddr_read(cpu.esp,2,4);
+	cpu.esp=cpu.esp+4;
+	cpu.cs.val=(uint16_t)vaddr_read(cpu.esp,2,4);
+	cpu.esp+=4;
+	cpu.eflags.val=vaddr_read(cpu.esp,2,4);
+	cpu.esp+=4;
+	return 0;
+}
