c9fca5b1d8622bc7781bc99e0c8340eaa95af381
tracer-pa-public <tracer@njuics.org>
2020-12-21 16:41:19 +0800

    > compile nemu testcase kernel > id 191220177 > user pa191220177 > uname Linux 32e3186b035e 4.15.0-45-generic #48~16.04.1-Ubuntu SMP Tue Jan 29 18:03:48 UTC 2019 x86_64 GNU/Linux > uptime 16:41:19 up 105 days,  8:04,  1 user,  load average: 3.42, 2.56, 2.45 > unitime 1608540079227 > 5e12d1fb99c87710e3bfb225d07ea574402d3458

--- nemu/src/cpu/intr.c
+++ nemu/src/cpu/intr.c
@@ -40,5 +40,7 @@ void raise_sw_intr(uint8_t intr_no)
 {
 	// return address is the next instruction
 	cpu.eip += 2;
+	printf("intr cpu.eip : %x\n", cpu.eip);
 	raise_intr(intr_no);
+	printf("intr cpu.eip : %x\n", cpu.eip);
 }
