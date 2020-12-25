169acba826cb4a5d1ec350f0031f17d50143d924
tracer-pa-public <tracer@njuics.org>
2020-12-25 16:37:29 +0800

    > clean > id 191220177 > user pa191220177 > uname Linux 32e3186b035e 4.15.0-45-generic #48~16.04.1-Ubuntu SMP Tue Jan 29 18:03:48 UTC 2019 x86_64 GNU/Linux > uptime 16:37:29 up 109 days,  8:00,  1 user,  load average: 2.20, 2.34, 2.39 > unitime > fd55b613dfdd54b5d40c7e2e3e474f7310559da5

--- nemu/src/cpu/instr/lidt.c
+++ nemu/src/cpu/instr/lidt.c
@@ -4,7 +4,7 @@ Put the implementations of `lidt' instructions here.
 */
 make_instr_func(lidt)
 {
-	OPERAND t;
+	/*OPERAND t;
 	t.data_size = data_size;
 	int len = 1;
 	len=len + modrm_rm(eip + 1, &t);
@@ -15,5 +15,17 @@ make_instr_func(lidt)
 	else{
 		cpu.idtr.base=laddr_read(t.addr+2,3);
 	}
-	return len;
+	return len;*/
+	opr_src.data_size = 16;   //16
+    opr_dest.data_size = 32;  //32
+    int len = 1;
+    len += modrm_rm(eip + 1, &opr_src);
+    opr_dest.type = opr_src.type;
+    opr_dest.addr = opr_src.addr + 2;
+    operand_read(&opr_src);
+    operand_read(&opr_dest);
+    print_asm_1("lidt", "", 6, &opr_src);
+    cpu.idtr.limit = opr_src.val;
+    cpu.idtr.base = data_size == 16 ? opr_dest.val & 0xffffff : opr_dest.val;
+    return len;
 }
