f8842dd22e5c056612d64df8d04c15e75d872088
tracer-pa-public <tracer@njuics.org>
2020-12-25 11:58:17 +0800

    > clean > id 191220177 > user pa191220177 > uname Linux 32e3186b035e 4.15.0-45-generic #48~16.04.1-Ubuntu SMP Tue Jan 29 18:03:48 UTC 2019 x86_64 GNU/Linux > uptime 11:58:17 up 109 days,  3:21,  2 users,  load average: 2.36, 2.43, 2.39 > unitime > 9bbcfb9fb54a8abc2a48fa2ce97efd7da6550489

--- nemu/src/cpu/instr/out.c
+++ nemu/src/cpu/instr/out.c
@@ -6,17 +6,17 @@
 /*
 Put the implementations of `out' instructions here.
 */
-make_instr_func(out_b) {
-	print_asm_0("out", "b", 1);
-	pio_write(DX, 1, AL);
-	return 1;
+make_instr_func(out_b)
+{
+    pio_write(DX, 1, AL);
+    return 1;
 }
 
-make_instr_func(out_v) {
-	print_asm_0("out", (data_size == 16)?"w":"l", 1);
-	if (data_size == 16)
+make_instr_func(out_v)
+{
+    if (data_size == 16)
         pio_write(DX, 2, AX);
     else
         pio_write(DX, 4, cpu.eax);
-	return 1;
+    return 1;
 }
