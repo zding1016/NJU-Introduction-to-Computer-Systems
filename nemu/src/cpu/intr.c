a5cc7b73900410965b02dc5dd8a4071b689d981f
tracer-pa-public <tracer@njuics.org>
2020-12-29 22:36:50 +0800

    > clean > id 191220177 > user pa191220177 > uname Linux 32e3186b035e 4.15.0-45-generic #48~16.04.1-Ubuntu SMP Tue Jan 29 18:03:48 UTC 2019 x86_64 GNU/Linux > uptime 22:36:50 up 113 days, 13:59,  1 user,  load average: 2.42, 2.78, 2.60 > unitime > b3c3d128d71a8c74ee6f78f119508c0d7bec1a72

--- nemu/src/cpu/intr.c
+++ nemu/src/cpu/intr.c
@@ -5,10 +5,7 @@
 void raise_intr(uint8_t intr_no)
 {
 #ifdef IA32_INTR
-	/*printf("Please implement raise_intr()");
-	fflush(stdout);
-	assert(0);*/
-	GateDesc IDTi;
+    GateDesc IDTi;
     IDTi.val[0] = laddr_read(cpu.idtr.base+(intr_no << 3), 4);
     IDTi.val[1] = laddr_read(cpu.idtr.base+(intr_no << 3)+4, 4);
     assert(IDTi.present == 1);
@@ -24,9 +21,9 @@ void raise_intr(uint8_t intr_no)
         if (cpu.cs.rpl > GDTi.privilege_level) {
             assert(0);
         }
-        push_help(cpu.eflags.val, 32);
-        push_help(cpu.cs.val, 32);
-        push_help(cpu.eip, 32);
+        push_(cpu.eflags.val, 32);
+        push_(cpu.cs.val, 32);
+        push_(cpu.eip, 32);
         if (IDTi.type == 14) cpu.eflags.IF = 0;
         cpu.cs.val = IDTi.selector;
         load_sreg(1);
@@ -39,7 +36,5 @@ void raise_sw_intr(uint8_t intr_no)
 {
 	// return address is the next instruction
 	cpu.eip += 2;
-	//printf("intr cpu.eip : %x\n", cpu.eip);
 	raise_intr(intr_no);
-	//printf("intr cpu.eip : %x\n", cpu.eip);
 }
