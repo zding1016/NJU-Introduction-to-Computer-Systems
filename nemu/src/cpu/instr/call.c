b1a066a9e610956e14f55ba667619045b1df89cc
tracer-pa-public <tracer@njuics.org>
2021-01-09 23:37:09 +0800

    > compile nemu testcase kernel > id 191220177 > user pa191220177 > uname Linux 32e3186b035e 4.15.0-45-generic #48~16.04.1-Ubuntu SMP Tue Jan 29 18:03:48 UTC 2019 x86_64 GNU/Linux > uptime 23:37:09 up 124 days, 14:59,  1 user,  load average: 2.34, 2.65, 2.55 > unitime 1610206629722 > 55517b265d68f812902eb7487a83e517a7ae4883

--- nemu/src/cpu/instr/call.c
+++ nemu/src/cpu/instr/call.c
@@ -3,27 +3,70 @@
 Put the implementations of `call' instructions here.
 */
 
-make_instr_func(call_near) {
-    cpu.eip += 1+data_size/8;
-    push_(cpu.eip, data_size);
-    OPERAND rel;
-    rel.type = OPR_IMM;
-    rel.sreg = SREG_CS;
-    rel.data_size = data_size;
-    rel.addr = eip+1;
-    operand_read(&rel);
-    cpu.eip = (cpu.eip+rel.val) & (0xFFFFFFFF >> (32-data_size));
+void push_eip()
+{
+    // if(data_size==16) 
+    // {
+    //     cpu.esp-=2;
+    //     opr_dest.val=sign_ext(cpu.eip,data_size);
+    // }
+    // else 
+    // {
+    //     cpu.esp-=4;
+    //     opr_dest.val=cpu.eip;
+    // }
+    cpu.esp-=4;
+    opr_dest.val=cpu.eip;
+    opr_dest.data_size=data_size;
+    opr_dest.addr=cpu.esp;
+    opr_dest.type=OPR_MEM;
+    opr_dest.sreg=SREG_SS;
+    operand_write(&opr_dest);
+}
+
+
+make_instr_func(call_near_indirect)
+{
+    int len=1;
+    len+=modrm_rm(eip+1,&opr_src);
+    opr_src.sreg=SREG_SS;
+    operand_read(&opr_src);
+    print_asm_1("call",opr_src.data_size == 8 ? "b" : (opr_src.data_size == 16 ? "w" : "l"),5,&opr_src);
+    cpu.eip+=len;
+    push_eip();
+    // if(data_size==16)
+    // {
+    //     cpu.eip=opr_src.val&0x0000FFFF;
+    // }else
+    // {
+    //     cpu.eip=opr_src.val;
+    // }
+    cpu.eip=opr_src.val;
     return 0;
 }
 
-make_instr_func(call_near_indirect) {
-    OPERAND rm;
-    rm.data_size = data_size;
-    int len = 1;
-    len += modrm_rm(eip+1, &rm);
-    operand_read(&rm);
-    cpu.eip += len;
-    push_(cpu.eip, data_size);
-    cpu.eip = rm.val & (0xFFFFFFFF >> (32-data_size));
+make_instr_func(call_near)
+{
+
+    //len+=modrm_rm(eip+1,&opr_src);
+    opr_src.type=OPR_IMM;
+    opr_src.addr=cpu.eip+1;
+    opr_src.data_size=32;
+    opr_src.sreg=SREG_SS;
+    operand_read(&opr_src);
+    print_asm_1("call",opr_src.data_size == 8 ? "b" : (opr_src.data_size == 16 ? "w" : "l"),5,&opr_src);
+    cpu.eip+=5;  //立即数长度
+    push_eip();
+    
+    // if(data_size==16)
+    // {
+    //     cpu.eip=(cpu.eip+opr_src.val)&0x0000FFFF;
+    // }
+    // else
+    // {
+    //     cpu.eip+=opr_src.val;
+    // }
+     cpu.eip+=opr_src.val;
     return 0;
+   
 }
