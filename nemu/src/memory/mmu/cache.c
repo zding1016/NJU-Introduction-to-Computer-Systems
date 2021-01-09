990977f2aca8dfc0ce59befa471c381668300cfd
tracer-pa-public <tracer@njuics.org>
2021-01-09 23:34:28 +0800

    > compile nemu testcase kernel > id 191220177 > user pa191220177 > uname Linux 32e3186b035e 4.15.0-45-generic #48~16.04.1-Ubuntu SMP Tue Jan 29 18:03:48 UTC 2019 x86_64 GNU/Linux > uptime 23:34:28 up 124 days, 14:57,  1 user,  load average: 2.50, 2.69, 2.54 > unitime 1610206468749 > 262d6d567ca81f2628c4f3f7681d560c7c804359

--- nemu/src/memory/mmu/cache.c
+++ nemu/src/memory/mmu/cache.c
@@ -1,110 +1,103 @@
 #include "memory/mmu/cache.h"
-#include <time.h>
+#include "memory/memory.h"
 #include <stdlib.h>
 
-static struct CacheLine {
-    bool valid;
-    uint16_t tag;
-    uint8_t data[64];  
-} cache_line[128][8];
-
-extern uint8_t hw_mem[];
-extern uint32_t hw_mem_read(paddr_t paddr, size_t len);
-extern void hw_mem_write(paddr_t paddr, size_t len, uint32_t data);
 
 // init the cache
+#ifdef CACHE_ENABLED
 void init_cache()
 {
-	// implement me in PA 3-1
-    memset(cache_line, 0, sizeof(cache_line));
+    for (int i = 0; i < CACHE_SIZE; i++)
+        cache[i].validbit = false;
+    // implement me in PA 3-1
 }
 
 // write data to cache
 void cache_write(paddr_t paddr, size_t len, uint32_t data)
 {
-	// implement me in PA 3-1
-    uint32_t paddr_group1 = (paddr >> 6) & 0x7F, paddr_row1 = 0, cache_addr1 = paddr & 0x3F;
-    uint16_t paddr_tag1 = paddr >> 13;
-    uint32_t paddr_group2 = ((paddr+len-1) >> 6) & 0x7F, paddr_row2 = 0;
-    uint16_t paddr_tag2 = (paddr+len-1) >> 13;
-    bool hit1 = false, hit2 = true;
-    for (; paddr_row1 < 8; ++paddr_row1) {
-        if (cache_line[paddr_group1][paddr_row1].valid && cache_line[paddr_group1][paddr_row1].tag == paddr_tag1) {
-            hit1 = true;
-            break;
-        }
-    }
-    if (cache_addr1+len-1 >= 64) {
-        hit2 = false;
-        for (; paddr_row2 < 8; ++paddr_row2) {
-            if (cache_line[paddr_group2][paddr_row2].valid && cache_line[paddr_group2][paddr_row2].tag == paddr_tag2) {
-                hit2 = true;
-                break;
+
+    uint32_t block_index = paddr & (0xffffffff >> (32 - BLOCK_INDEX_LEN)); //块内地址
+    uint32_t group_index = (paddr >> BLOCK_INDEX_LEN) & (0xffffffff >> (32 - GROUP_INDEX_LEN));
+    //组号
+    uint32_t label = (paddr >> (BLOCK_INDEX_LEN + GROUP_INDEX_LEN)); //标记
+    uint32_t start = group_index * GROUP_SIZE;                       //组起始地址
+
+    memcpy(hw_mem + paddr, &data, len);
+    for (int i = start; i < start + GROUP_SIZE; i++)
+    {
+        //命中cache
+        if (cache[i].validbit && cache[i].label == label)
+        {
+            if (block_index + len > BLOCK_SIZE) //数据分开写入
+            {
+
+                size_t len1 = BLOCK_SIZE - block_index;
+                size_t len2 = len - len1;
+                uint32_t data1 = data;               //低位
+                uint32_t data2 = data >> (8 * len1); //高位
+                cache_write(paddr, len1, data1);
+                cache_write(paddr + len1, len2, data2);
+            }
+            else
+            {
+                memcpy(&cache[i].block[block_index], &data, len); //写cache
             }
         }
     }
-    if (hit1) {
-        if (cache_addr1+len-1 >= 64) {
-            memcpy(cache_line[paddr_group1][paddr_row1].data+cache_addr1, &data, 64-cache_addr1);
-        } else {
-            memcpy(cache_line[paddr_group1][paddr_row1].data+cache_addr1, &data, len);
-        }
-    }
-    if (hit2 && cache_addr1+len-1 >= 64) {
-        uint32_t data_high = data >> ((64-cache_addr1) << 3);
-        memcpy(cache_line[paddr_group2][paddr_row2].data, &data_high, cache_addr1+len-64);
-    }
-    hw_mem_write(paddr, len, data);
+
+    //不命中
+    //不做操作
 }
 
-// read data from cache
 uint32_t cache_read(paddr_t paddr, size_t len)
 {
-	// implement me in PA 3-1
-	uint32_t paddr_group1 = (paddr >> 6) & 0x7F, paddr_row1 = 0, cache_addr1 = paddr & 0x3F;
-    uint16_t paddr_tag1 = paddr >> 13;
-    uint32_t paddr_group2 = ((paddr+len-1) >> 6) & 0x7F, paddr_row2 = 0;
-    uint16_t paddr_tag2 = (paddr+len-1) >> 13;
-    bool hit1 = false, hit2 = true;
-    for (; paddr_row1 < 8; ++paddr_row1) {
-        if (cache_line[paddr_group1][paddr_row1].valid && cache_line[paddr_group1][paddr_row1].tag == paddr_tag1) {
-            hit1 = true;
-            break;
-        }
+
+    uint32_t block_index = paddr & (0xffffffff >> (32 - BLOCK_INDEX_LEN)); //块内地址
+    if (block_index + len > BLOCK_SIZE)                                    //block越界,分开读取
+    {
+        size_t len1 = BLOCK_SIZE - block_index;
+        size_t len2 = len - len1;
+        uint32_t data1 = cache_read(paddr, len1);        //低位
+        uint32_t data2 = cache_read(paddr + len1, len2); //高位
+        uint32_t data = (data2 << len1 * 8) + data1;
+        return data;
     }
-    if (cache_addr1+len-1 >= 64) {
-        hit2 = false;
-        for (; paddr_row2 < 8; ++paddr_row2) {
-            if (cache_line[paddr_group2][paddr_row2].valid && cache_line[paddr_group2][paddr_row2].tag == paddr_tag2) {
-                hit2 = true;
-                break;
+    else
+    {
+        uint32_t group_index = (paddr >> BLOCK_INDEX_LEN) & (0xffffffff >> (32 - GROUP_INDEX_LEN));
+        //组号
+        uint32_t label = (paddr >> (BLOCK_INDEX_LEN + GROUP_INDEX_LEN)); //标记
+        uint32_t start = group_index * GROUP_SIZE;                       //组起始地址
+        uint32_t data = 0;
+        for (int i = start; i < start + GROUP_SIZE; i++)
+        {
+            //命中cache
+            if (cache[i].validbit && cache[i].label == label)
+            {
+                memcpy(&data, &cache[i].block[block_index], len); //block读取数据
+                return data;
             }
         }
-    }
-    uint32_t ret = 0;
-    if (hit1 && hit2) {
-        if (cache_addr1+len-1 >= 64) {
-            uint32_t r1 = 0, r2 = 0;
-            memcpy(&r1, cache_line[paddr_group1][paddr_row1].data+cache_addr1, 64-cache_addr1);
-            memcpy(&r2, cache_line[paddr_group2][paddr_row2].data, cache_addr1+len-64);
-            ret = r1 | (r2 << ((64-cache_addr1) << 3));
-        } else {
-            memcpy(&ret, cache_line[paddr_group1][paddr_row1].data+cache_addr1, len);
+        //没有命中cache
+        for (int i = start; i < start + GROUP_SIZE; i++)
+        {
+            //group有空位
+            if (!cache[i].validbit)
+            {
+                memcpy(cache[i].block, (hw_mem + ((paddr / BLOCK_SIZE) * BLOCK_SIZE)), BLOCK_SIZE); 
+                cache[i].validbit = true;
+                cache[i].label = label;
+                memcpy(&data, &cache[i].block[block_index], len); //重新从block读取数据
+                return data;
+            }
         }
-    } else {
-        ret = hw_mem_read(paddr, len);
-        srand(time(0));
-        paddr_row1 = rand()%8;
-        cache_line[paddr_group1][paddr_row1].valid = true;
-        cache_line[paddr_group1][paddr_row1].tag = paddr_tag1;
-        memcpy(cache_line[paddr_group1][paddr_row1].data, hw_mem+(paddr_tag1 << 13)+(paddr_group1 << 6), 64);
-        if (cache_addr1+len-1 >= 64) {
-            paddr_row2 = rand()%8;
-            cache_line[paddr_group2][paddr_row2].valid = true;
-            cache_line[paddr_group2][paddr_row2].tag = paddr_tag2;
-            memcpy(cache_line[paddr_group2][paddr_row2].data, hw_mem+(paddr_tag2 << 13)+(paddr_group2 << 6), 64);
-        }
+        //group无空位
+        int i = start + rand() % GROUP_SIZE;
+        memcpy(cache[i].block, (hw_mem + ((paddr / BLOCK_SIZE) * BLOCK_SIZE)), BLOCK_SIZE);
+        cache[i].validbit = true;
+        cache[i].label = label;
+        memcpy(&data, &cache[i].block[block_index], len); 
+        return data;
     }
-	return ret;
 }
-
+#endif
