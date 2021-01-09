13a158f5235c6e2498586c781e7d8a03ce62d0e1
tracer-pa-public <tracer@njuics.org>
2021-01-09 23:42:53 +0800

    > compile nemu testcase kernel > id 191220177 > user pa191220177 > uname Linux 32e3186b035e 4.15.0-45-generic #48~16.04.1-Ubuntu SMP Tue Jan 29 18:03:48 UTC 2019 x86_64 GNU/Linux > uptime 23:42:53 up 124 days, 15:05,  1 user,  load average: 2.15, 2.58, 2.58 > unitime 1610206973040 > 6492ed9217b959d41f9ef9909d23b379f10221ce

--- nemu/src/cpu/instr/cmov.c
+++ nemu/src/cpu/instr/cmov.c
@@ -25,3 +25,42 @@ make_instr_impl_2op_cc(cmov, rm, r, v, o)
 make_instr_impl_2op_cc(cmov, rm, r, v, p)
 make_instr_impl_2op_cc(cmov, rm, r, v, s)
 make_instr_impl_2op_cc(cmov, rm, r, v, e)
+
+
+make_instr_func(mov_c2r_l) {
+    int len = 1;
+    OPERAND r, rm;
+    r.data_size = 32;
+    rm.data_size = 32;
+    len += modrm_r_rm(eip+1, &r, &rm);
+#ifdef IA32_SEG
+    switch (rm.addr) {
+        case 0: r.val = cpu.cr0.val; break;
+#ifdef IA32_PAGE
+        case 3: r.val = cpu.cr3.val; break;
+#endif
+        default: assert(0);
+    }
+    operand_write(&r);
+#endif
+    return len;
+}
+
+make_instr_func(mov_r2c_l) {
+    int len = 1;
+    OPERAND r, rm;
+    r.data_size = 32;
+    rm.data_size = 32;
+    len += modrm_r_rm(eip+1, &r, &rm);
+    operand_read(&rm);
+#ifdef IA32_SEG
+    switch (r.addr) {
+        case 0: cpu.cr0.val = rm.val; break;
+#ifdef IA32_PAGE
+        case 3: cpu.cr3.val = rm.val; break;
+#endif
+        default: assert(0);
+    }
+#endif
+    return len;
+}
