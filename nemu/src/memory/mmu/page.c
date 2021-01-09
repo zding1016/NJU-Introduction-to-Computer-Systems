ee7c4ef41d9c37eafd88ddcf3f20bdf7eeddb891
tracer-pa-public <tracer@njuics.org>
2021-01-09 23:34:58 +0800

    > compile nemu testcase kernel > id 191220177 > user pa191220177 > uname Linux 32e3186b035e 4.15.0-45-generic #48~16.04.1-Ubuntu SMP Tue Jan 29 18:03:48 UTC 2019 x86_64 GNU/Linux > uptime 23:34:58 up 124 days, 14:57,  1 user,  load average: 2.85, 2.77, 2.57 > unitime 1610206498386 > bae53b82c1ec3689e4601f9bc754a1f1571ab76c

--- nemu/src/memory/mmu/page.c
+++ nemu/src/memory/mmu/page.c
@@ -6,13 +6,13 @@ paddr_t page_translate(laddr_t laddr)
 {
 #ifndef TLB_ENABLED
 #ifdef IA32_PAGE
-	PDE dirEntry;
-	dirEntry.val = paddr_read((cpu.cr3.pdbr << 12) | (((laddr >> 22) & 0x000003FF) << 2), 4);
-	assert(dirEntry.present == 1);
-	PTE tableEntry;
-	tableEntry.val = paddr_read((dirEntry.page_frame << 12) | (((laddr >> 12) & 0x000003FF) << 2), 4);
-	assert(tableEntry.present == 1);
-	return (tableEntry.page_frame << 12) | (laddr & 0x00000FFF);
+	PDE pde;
+	pde.val = paddr_read((cpu.cr3.pdbr << 12) | (((laddr >> 22) & 0x000003FF) << 2), 4);
+	assert(pde.present == 1);
+	PTE pte;
+	pte.val = paddr_read((pde.page_frame << 12) | (((laddr >> 12) & 0x000003FF) << 2), 4);
+	assert(pte.present == 1);
+	return (pte.page_frame << 12) | (laddr & 0x00000FFF);
 #else
     return 0;
 #endif
