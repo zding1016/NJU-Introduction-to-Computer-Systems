2078f373c643f6224f25bfec208c7a315cc3ebdc
tracer-pa-public <tracer@njuics.org>
2021-01-09 23:38:28 +0800

    > compile nemu testcase kernel > id 191220177 > user pa191220177 > uname Linux 32e3186b035e 4.15.0-45-generic #48~16.04.1-Ubuntu SMP Tue Jan 29 18:03:48 UTC 2019 x86_64 GNU/Linux > uptime 23:38:28 up 124 days, 15:01,  1 user,  load average: 2.44, 2.67, 2.57 > unitime 1610206708277 > a84f87f4b90b6449d0b18a1d919788816d8d3145

--- nemu/src/cpu/instr/call.c
+++ nemu/src/cpu/instr/call.c
@@ -5,68 +5,40 @@ Put the implementations of `call' instructions here.
 
 void push_eip()
 {
-    // if(data_size==16) 
-    // {
-    //     cpu.esp-=2;
-    //     opr_dest.val=sign_ext(cpu.eip,data_size);
-    // }
-    // else 
-    // {
-    //     cpu.esp-=4;
-    //     opr_dest.val=cpu.eip;
-    // }
-    cpu.esp-=4;
-    opr_dest.val=cpu.eip;
-    opr_dest.data_size=data_size;
-    opr_dest.addr=cpu.esp;
-    opr_dest.type=OPR_MEM;
-    opr_dest.sreg=SREG_SS;
+    cpu.esp -= 4;
+    opr_dest.val = cpu.eip;
+    opr_dest.data_size = data_size;
+    opr_dest.addr = cpu.esp;
+    opr_dest.type = OPR_MEM;
+    opr_dest.sreg = SREG_SS;
     operand_write(&opr_dest);
 }
 
-
 make_instr_func(call_near_indirect)
 {
-    int len=1;
-    len+=modrm_rm(eip+1,&opr_src);
-    opr_src.sreg=SREG_SS;
+    int len = 1;
+    len += modrm_rm(eip + 1, &opr_src);
+    opr_src.sreg = SREG_SS;
     operand_read(&opr_src);
-    print_asm_1("call",opr_src.data_size == 8 ? "b" : (opr_src.data_size == 16 ? "w" : "l"),5,&opr_src);
-    cpu.eip+=len;
+    print_asm_1("call", opr_src.data_size == 8 ? "b" : (opr_src.data_size == 16 ? "w" : "l"), 5, &opr_src);
+    cpu.eip += len;
     push_eip();
-    // if(data_size==16)
-    // {
-    //     cpu.eip=opr_src.val&0x0000FFFF;
-    // }else
-    // {
-    //     cpu.eip=opr_src.val;
-    // }
-    cpu.eip=opr_src.val;
+    cpu.eip = opr_src.val;
     return 0;
 }
 
 make_instr_func(call_near)
 {
 
-    //len+=modrm_rm(eip+1,&opr_src);
-    opr_src.type=OPR_IMM;
-    opr_src.addr=cpu.eip+1;
-    opr_src.data_size=32;
-    opr_src.sreg=SREG_SS;
+    opr_src.type = OPR_IMM;
+    opr_src.addr = cpu.eip + 1;
+    opr_src.data_size = 32;
+    opr_src.sreg = SREG_SS;
     operand_read(&opr_src);
-    print_asm_1("call",opr_src.data_size == 8 ? "b" : (opr_src.data_size == 16 ? "w" : "l"),5,&opr_src);
-    cpu.eip+=5;  //立即数长度
+    print_asm_1("call", opr_src.data_size == 8 ? "b" : (opr_src.data_size == 16 ? "w" : "l"), 5, &opr_src);
+    cpu.eip += 5;
     push_eip();
-    
-    // if(data_size==16)
-    // {
-    //     cpu.eip=(cpu.eip+opr_src.val)&0x0000FFFF;
-    // }
-    // else
-    // {
-    //     cpu.eip+=opr_src.val;
-    // }
-     cpu.eip+=opr_src.val;
+
+    cpu.eip += opr_src.val;
     return 0;
-   
 }
--- nemu/src/cpu/instr/leave.c
+++ nemu/src/cpu/instr/leave.c
@@ -2,8 +2,16 @@
 /*
 Put the implementations of `leave' instructions here.
 */
-make_instr_func(leave) {
-    cpu.esp = cpu.ebp;
-    cpu.ebp = pop_(data_size);
+make_instr_func(leave)
+{
+    cpu.esp=cpu.ebp;
+    opr_dest.type=OPR_MEM;
+    opr_dest.addr=cpu.esp;
+    opr_dest.data_size=32;
+    opr_dest.sreg=SREG_SS;
+    operand_read(&opr_dest); 
+    print_asm_1("leave","",1,&opr_dest);
+    cpu.ebp=opr_dest.val;
+    cpu.esp+=4;
     return 1;
 }
